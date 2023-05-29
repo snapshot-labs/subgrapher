d497312c2d2b35bde59c9066e4a6260898b7a270
ChaituVR <yourchaitu@gmail.com>
2023-05-29 12:18:59 +0530

    Fix EE when there is error

--- src/helpers/ee.ts
+++ src/helpers/ee.ts
@@ -6,8 +6,8 @@ eventEmitter.setMaxListeners(1000); // https://stackoverflow.com/a/26176922
 
 export default async function serve(id, action, args) {
   const key = sha256(id);
-  return new Promise(async (resolve, reject) => {
-    eventEmitter.once(key, data => (data.error ? reject(data.e) : resolve(data)));
+  return new Promise(async resolve => {
+    eventEmitter.once(key, data => resolve(data));
     if (eventEmitter.listenerCount(key) === 1) {
       try {
         eventEmitter.emit(key, await action(...args));

12b46879fa4d4565f8d0f46db1eac61406b827d6
ChaituVR <yourchaitu@gmail.com>
2023-07-20 04:01:51 +0530

    Fix event emitter once function

--- src/helpers/ee.ts
+++ src/helpers/ee.ts
@@ -7,7 +7,12 @@ eventEmitter.setMaxListeners(10000); // https://stackoverflow.com/a/26176922
 export default async function serve(id, action, args) {
   const key = sha256(id);
   return new Promise(async resolve => {
-    eventEmitter.once(key, data => resolve(data));
+    eventEmitter.on(key, data => {
+      resolve(data);
+      const listeners = eventEmitter.listeners(key);
+      // @ts-ignore
+      eventEmitter.removeListener(key, listeners[0]);
+    });
     if (eventEmitter.listenerCount(key) === 1) {
       try {
         eventEmitter.emit(key, await action(...args));

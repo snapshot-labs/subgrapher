e93342f67fe17731393f450bfa5681215ff74062
ChaituVR <yourchaitu@gmail.com>
2024-06-11 23:14:47 +0530

    fix tests

--- test/e2e/index.test.ts
+++ test/e2e/index.test.ts
@@ -1,8 +1,7 @@
 import request from 'supertest';
 
 const HOST = `http://localhost:${process.env.PORT || 3003}`;
-const PROXIED_URL =
-  '/subgrapher.snapshot.org/api.thegraph.com/subgraphs/name/snapshot-labs/snapshot-arbitrum';
+const PROXIED_URL = '/delegation/42161';
 
 describe('api', () => {
   describe('send a request to the proxied service', () => {
@@ -50,34 +49,20 @@ describe('api', () => {
       });
 
       it('should return a 400 on invalid query', async () => {
-        const response = await request(HOST).post('/gateway-test').send({ query: 'sdkfksdf' });
+        const response = await request(HOST).post(PROXIED_URL).send({ query: 'sdkfksdf' });
 
         expect(response.statusCode).toBe(400);
         expect(response.body.errors[0].message).toContain('Query parse error');
       });
 
-      it('should return a 500 on invalid proxied url', async () => {
+      it('should return a 400 on invalid proxied url', async () => {
         const response = await request(HOST).post('/gateway-test').send(payload);
 
-        expect(response.statusCode).toBe(500);
+        expect(response.statusCode).toBe(400);
         expect(response.body).toEqual({
           errors: [
             {
-              message:
-                'request to https://gateway-test failed, reason: getaddrinfo ENOTFOUND gateway-test'
-            }
-          ]
-        });
-      });
-
-      it('should return a 500 on network error to proxied url', async () => {
-        const response = await request(HOST).post('/snapshot.org').send(payload);
-
-        expect(response.statusCode).toBe(500);
-        expect(response.body).toEqual({
-          errors: [
-            {
-              message: 'Unable to connect to https://snapshot.org, code: 405'
+              message: 'Invalid subgraph'
             }
           ]
         });
@@ -86,13 +71,21 @@ describe('api', () => {
       it('should return a 400 on graphql error', async () => {
         const response = await request(HOST).post(PROXIED_URL).send({
           query:
-            'query { delegations (where: {space_in: ["", "stgdao.eth", "stgdao"]}, first: 1, skip: 1000000000) { delegator space delegate } }'
+            'query { delegation (where: {space_in: ["", "stgdao.eth", "stgdao"]}, first: 1, skip: 1000000000) { delegator space delegate } }'
         });
 
         expect(response.statusCode).toBe(400);
         expect(response.body).toEqual({
           errors: [
-            { message: expect.stringContaining('The `skip` argument must be between 0 and') }
+            {
+              locations: [
+                {
+                  column: 0,
+                  line: 0
+                }
+              ],
+              message: expect.stringContaining('No value provided for required argument: `id`')
+            }
           ]
         });
       });
--- test/unit/utils.test.ts
+++ test/unit/utils.test.ts
@@ -1,19 +1,19 @@
 import { buildURL } from '../../src/utils';
 
 describe('buildURL general use case', () => {
-  it('if a URL is passed, return error', () => {
+  it('returns error if a URL is passed', () => {
     expect(() => buildURL('http://localhost:3000')).toThrowError('Invalid subgraph');
   });
 });
 
 describe('buildURL delegation cases', () => {
-  it('if an invalid network is passed, return error', () => {
+  it('returns error if an invalid network is passed', () => {
     expect(() => buildURL('delegation/2')).toThrowError('Invalid network');
   });
-  it('if no network is passed, return error', () => {
+  it('returns error if no network is passed', () => {
     expect(() => buildURL('delegation/')).toThrowError('Invalid network');
   });
-  it('if a delegation subgraph URL is passed, return the subgraph URL', () => {
+  it('returns the subgraph URL if a delegation subgraph URL is passed', () => {
     expect(buildURL('delegation/1')).toBe(
       `https://gateway-arbitrum.network.thegraph.com/api/${process.env.SUBGRAPH_KEY}/subgraphs/id/4YgtogVaqoM8CErHWDK8mKQ825BcVdKB8vBYmb4avAQo`
     );
@@ -21,7 +21,7 @@ describe('buildURL delegation cases', () => {
       `https://gateway-arbitrum.network.thegraph.com/api/${process.env.SUBGRAPH_KEY}/subgraphs/id/CAsVTyvLA6vnvDX9zgXifmi3wFM8GYeNHvRpxsABvbYL`
     );
   });
-  it('if an extra slash is passed, should still return subgraph URL', () => {
+  it('should still return subgraph URL if an extra slash is passed', () => {
     expect(buildURL('delegation/1/')).toBe(
       `https://gateway-arbitrum.network.thegraph.com/api/${process.env.SUBGRAPH_KEY}/subgraphs/id/4YgtogVaqoM8CErHWDK8mKQ825BcVdKB8vBYmb4avAQo`
     );
@@ -29,33 +29,31 @@ describe('buildURL delegation cases', () => {
 });
 
 describe('buildURL upgraded subgraph cases', () => {
-  it('if an invalid network is passed, return error', () => {
+  it('returns error if an invalid network is passed', () => {
     expect(() => buildURL('subgraph/2')).toThrowError('Invalid network');
   });
-  it('if no network is passed, return error', () => {
+  it('returns error if no network is passed', () => {
     expect(() => buildURL('subgraph/')).toThrowError('Invalid network');
   });
-  it('if an invalid subgraph is passed, return error', () => {
+  it('returns error if an invalid subgraph is passed', () => {
     expect(() => buildURL('subgraph/arbitrum')).toThrowError('Invalid subgraph');
   });
-  it('if no subgraph is passed, return error', () => {
+  it('returns error if no subgraph is passed', () => {
     expect(() => buildURL('subgraph/arbitrum/')).toThrowError('Invalid subgraph');
   });
-  it('if a upgraded snapshot subgraph URL is passed, return the subgraph URL', () => {
+  it('returns the subgraph URL if a upgraded snapshot subgraph URL is passed', () => {
     expect(buildURL('subgraph/arbitrum/EYCKATKGBKLWvSfwvBjzfCBmGwYNdVkduYXVivCsLRFu')).toBe(
       `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NON_RESTRICTED_SUBGRAPH_KEY}/subgraphs/id/EYCKATKGBKLWvSfwvBjzfCBmGwYNdVkduYXVivCsLRFu`
     );
   });
-  it('if a different upgraded snapshot subgraph URL is passed, return the subgraph URL', () => {
+  it('returns the subgraph URL if a different upgraded snapshot subgraph URL is passed', () => {
     expect(buildURL('subgraph/mainnet/2XuuZyGrxw72keXKfeHQW7yaGqVa7dyoghkgdGMdC6Az')).toBe(
       `https://gateway.network.thegraph.com/api/${process.env.NON_RESTRICTED_SUBGRAPH_KEY}/subgraphs/id/2XuuZyGrxw72keXKfeHQW7yaGqVa7dyoghkgdGMdC6Az`
     );
   });
-  it('if an extra slash is passed, should still return subgraph URL', () => {
+  it('should still return subgraph URL if an extra slash is passed', () => {
     expect(buildURL('subgraph/arbitrum/EYCKATKGBKLWvSfwvBjzfCBmGwYNdVkduYXVivCsLRFu/')).toBe(
       `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NON_RESTRICTED_SUBGRAPH_KEY}/subgraphs/id/EYCKATKGBKLWvSfwvBjzfCBmGwYNdVkduYXVivCsLRFu`
     );
   });
 });
-
-
